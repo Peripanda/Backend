ceddca6db5d784730acac6491f5fbb1b813dfc86
Joaquin sanchez <jasanchez1@uc.cl>
2022-05-29 23:53:37 -0400

    chore: test balance

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -9,6 +9,14 @@ describe('Get Health Check', () => {
   });
 });
 
+describe('Get user by ID', () => {
+  it('Health check', async () => {
+    const res = await request(app)
+      .get('/users/1');
+    expect(res.statusCode).toEqual(500);
+  });
+});
+
 describe('Signup', () => {
   it('Happy path signup', async () => {
     const res = await request(app)
@@ -19,6 +27,17 @@ describe('Signup', () => {
         username: 'jasanchez1',
       });
     expect(res.statusCode).toEqual(400);
+  });
+});
+
+describe('Update Balance', () => {
+  it('Happy path balance', async () => {
+    const res = await request(app)
+      .patch('/users/signup')
+      .send({
+        delta: 100,
+      });
+    expect(res.statusCode).toEqual(500);
   });
 });
 
