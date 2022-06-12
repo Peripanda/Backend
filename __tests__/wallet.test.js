641730f3e950c7e94576a1a5f1c8917c360b260f
diaztunjano <dfdiaz1@uc.cl>
2022-06-11 22:26:49 -0400

    feat(wallet-tests): add updating balance tests

--- __tests__/wallet.test.js
+++ __tests__/wallet.test.js
@@ -2,21 +2,21 @@ const request = require('supertest');
 const app = require('../bin/server');
 
 // Positive update
-describe.skip('Updating balance with proper amount', () => {
+describe('Updating balance with proper amount', () => {
   test('Happy path balance', async () => {
     const res = await request(app).patch('/users/1/balance').send({
       delta: 100,
     });
-    expect(res.statusCode).toEqual(404);
+    expect(res.statusCode).toEqual(200);
   });
 });
 
 // Negative update
-describe.skip('When taking funds exceeds balance', () => {
+describe('When taking funds exceeds balance', () => {
   test('Happy path balance', async () => {
     const res = await request(app).patch('/users/1/balance').send({
-      delta: 100,
+      delta: -100000000000,
     });
-    expect(res.statusCode).toEqual(404);
+    expect(res.statusCode).toEqual(400);
   });
 });
