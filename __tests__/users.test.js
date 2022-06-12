96f177cda216daab8a9959c4e0aabe6d09a47c8f
diaztunjano <dfdiaz1@uc.cl>
2022-06-11 22:23:55 -0400

    feat(tests): add tests for users endpoints, signin, signup and checking if user is/isn't in DB

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -1,63 +1,51 @@
 const request = require('supertest');
 const app = require('../bin/server');
 
-// Checking signup for existing user in DB
+// Checking signup
 describe('Signup', () => {
   it('Happy path signup', async () => {
+    expect.assertions(1);
     const res = await request(app).post('/users/signup').send({
-      email: 'dave112321@email.com',
-      password: '.Abcd123rejkbd1!!!!!',
-      username: 'dfdiaz33123',
+      email: `${Math.random().toString(36).slice(2, 7)}@email.com`,
+      password: `${Math.random().toString(36).slice(2, 14)}A123!!`,
+      username: `${Math.random().toString(36).slice(2, 7)}`,
+    });
+    await expect(res.statusCode).toEqual(200);
+  });
+});
+
+// // Check existing user in DB
+describe('Get user by ID', () => {
+  it('Health check', async () => {
+    const res = await request(app).get('/users/1');
+    expect(res.statusCode).toEqual(200);
+  });
+});
+
+// New user
+describe('New user creation and login', () => {
+  it('Happy path signin', async () => {
+    const newEmail = `${Math.random().toString(36).slice(2, 7)}@email.com`;
+    const newPassword = `${Math.random().toString(36).slice(2, 14)}A123!!`;
+    const newUsername = `${Math.random().toString(36).slice(2, 7)}`;
+    await request(app).post('/users/signup').send({
+      email: newEmail,
+      password: newPassword,
+      username: newUsername,
+    });
+    const res = await request(app).post('/users/signin').send({
+      email: newEmail,
+      password: newPassword,
+      username: newUsername,
     });
     expect(res.statusCode).toEqual(200);
   });
 });
 
-// // User in DB
-// describe('Get user by ID', () => {
-//   it('Health check', async () => {
-//     const res = await request(app).get('/users/1');
-//     expect(res.statusCode).toEqual(500);
-//   });
-// });
-
-// New user
-// describe('New user creation and login', () => {
-//   const userData = {
-//     email: 'elwajas1@uc.cl',
-//     username: 'elwajeiro1',
-//     password: 'wajitrex1',
-//   };
-
-//   beforeAll(async (createUserUseCase) => {
-//     const user = await createUserUseCase(userData);
-//   });
-
-//   it('Happy path signup', async () => {
-//     const res = await request(app).post('/users/signin').send({
-//     email: 'elwajas1@uc.cl',
-//     username: 'elwajeiro1',
-//     password: 'wajitrex1',
-//   });
-//     expect(res.statusCode).toEqual(500);
-//   });
-// });
-
-// describe('Signin', () => {
-//   it('Happy path signin', async () => {
-//     const res = await request(app).post('/users/signin').send({
-//       email: 'joaquinUS130@email.com',
-//       password: '.Abcdhjkbd1!!!!!',
-//       username: 'jasanchez1',
-//     });
-//     expect(res.statusCode).toEqual(500);
-//   });
-// });
-
 // // User not in DB
-// describe('Get user by ID', () => {
-//   it('Health check', async () => {
-//     const res = await request(app).get('/users/100');
-//     expect(res.statusCode).toEqual(500);
-//   });
-// });
+describe('Get user by ID', () => {
+  it('Health check', async () => {
+    const res = await request(app).get('/users/100');
+    expect(res.statusCode).toEqual(404);
+  });
+});
